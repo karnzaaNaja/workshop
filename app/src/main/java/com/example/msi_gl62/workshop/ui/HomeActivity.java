d8c530358f3e1263d7f4bd661204d97b6069354d
nawakarn <karn939.n@gmail.com>
2018-09-24 01:19:31 +0700

    commit code

--- app/src/main/java/com/example/msi_gl62/workshop/ui/HomeActivity.java
+++ app/src/main/java/com/example/msi_gl62/workshop/ui/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.msi_gl62.workshop.ui;
 
 import android.content.Intent;
+
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +11,8 @@ import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -16,6 +21,10 @@ import com.example.msi_gl62.workshop.R;
 import com.example.msi_gl62.workshop.adapter.AdapterListView;
 import com.example.msi_gl62.workshop.model.UserModel;
 import com.example.msi_gl62.workshop.sqldatabase.DatabaseHelper;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +32,8 @@ public class HomeActivity extends AppCompatActivity {
     private AdapterListView adapterListView;
     private List<UserModel> readUser;
     private ListView listView;
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +51,9 @@ public class HomeActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String textName = bundle.getString("name");
         String textPassword = bundle.getString("password");
-        Toast.makeText(HomeActivity.this,"User : "+ textName,Toast.LENGTH_SHORT).show();
+        Toast.makeText(HomeActivity.this,"User : "+ textName+" "+" password : "+ textPassword,Toast.LENGTH_SHORT).show();
+
+        floating();
 
     }
 
@@ -49,6 +62,58 @@ public class HomeActivity extends AppCompatActivity {
         super.onResume();
         getUser();
     }
+
+
+    private  void floating(){
+        materialDesignFAM = findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton1 = findViewById(R.id.material_design_floating_action_menu_item1);
+        floatingActionButton2 = findViewById(R.id.material_design_floating_action_menu_item2);
+        floatingActionButton3 = findViewById(R.id.material_design_floating_action_menu_item3);
+
+        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                View bottomSheetView = getLayoutInflater().inflate(R.layout.buttom_sheet_layout, null);
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(HomeActivity.this);
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+                final EditText editTextAddress = bottomSheetView.findViewById(R.id.edtTextAddress);
+                final EditText editTextSubject = bottomSheetView.findViewById(R.id.edtTextEmail);
+                final EditText edtTextEmail = bottomSheetView.findViewById(R.id.edtTextEmail);
+                Button btnSendEmail = bottomSheetView.findViewById(R.id.btnSenadEmail);
+                btnSendEmail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/email");
+                        intent.putExtra(Intent.EXTRA_EMAIL
+                                , new String[] {editTextAddress.getText().toString()});
+                        intent.putExtra(Intent.EXTRA_SUBJECT
+                                , editTextSubject.getText().toString());
+                        intent.putExtra(Intent.EXTRA_TEXT
+                                , edtTextEmail.getText().toString());
+                        startActivity(Intent.createChooser(intent, "Send email with"));
+                    }
+                });
+
+            }
+        });
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this,"onClick image ",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this,"onClick edit ",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+
 
     private void getUser() {
         DatabaseHelper helper = new DatabaseHelper(this);
